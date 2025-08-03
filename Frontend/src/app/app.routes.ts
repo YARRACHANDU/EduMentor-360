d39c26ed2dc6340c1f180e4e1ec4bf4555c0c61f
PuttaPramod <pramodkumarputta06@gmail.com>
2025-08-03 17:50:23 +0530

    pramod

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
-import path from 'path';
 import { Login } from './login/login.component';
-import { App } from './app.component';
 import { Home } from './home/home.component';
 export const routes: Routes = [
-      { path: '', component: App },         // Default route
-     { path: 'login', component: Login }, 
-     { path: 'home', component: Home}
+  { path: '', component: Home },         // Default route pointing to Home component
+  { path: 'login', component: Login }, 
+  { path: 'home', component: Home}
 ];
