90ae232df23aaac513d9a9b7666f74be227a2687
PuttaPramod <pramodkumarputta06@gmail.com>
2025-08-04 21:25:51 +0530

    pk

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -4,8 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-login',
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class Login {
+  ngAfterViewInit() {
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
 
+    if (signUpButton && container) {
+      signUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+      });
+    }
+
+    if (signInButton && container) {
+      signInButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+      });
+    }
+  }
 }
