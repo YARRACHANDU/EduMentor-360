887abb931e353378571fde7f02fecd1967e5bd0b
PuttaPramod <pramodkumarputta06@gmail.com>
2025-08-23 19:49:58 +0530

    pk

--- Frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Login } from './login/login.component';
-import { Home } from './home/home.component';
-export const routes: Routes = [
-  { path: '', component: Home },         // Default route pointing to Home component
-  { path: 'login', component: Login },
-  { path: 'home', component: Home}
-];
